df0ccc1279434b3ae33c2b601cb6ddb864a73ab2
Ilya Dymshits <ilya@devlion.co>
2019-03-13 10:32:33 +0200

    (update) fix rtl styling

--- amd/src/ajax.js
+++ amd/src/ajax.js
@@ -1,112 +1,122 @@
 define([
-    'core/yui',
-    'filter_teamwork/popup',
-    'filter_teamwork/loading',
+  'core/yui',
+  'filter_teamwork/popup',
+  'filter_teamwork/loading',
 
-], function (Y, popup, loadingIcon) {
-    `use strict`;
+], function(Y, popup, loadingIcon) {
+`use strict`;
 
-    let ajax = {
+  let ajax = {
 
-        url: '/filter/teamwork/ajax/ajax.php',
+    url: '/filter/teamwork/ajax/ajax.php',
 
-        data: '',
+    data: '',
 
-        sesskey: M.cfg.sesskey,
+    sesskey: M.cfg.sesskey,
 
-        send: function () {
-            this.data.sesskey = this.sesskey;
+    send: function(){
+      this.data.sesskey = this.sesskey;
 
-            Y.io(M.cfg.wwwroot + this.url, {
-                method: 'POST',
-                data: this.data,
-                headers: {},
-                on: {
-                    success: function (id, response) {
-                    },
-                    failure: function () {
-                        popup.error();
-                    }
+      Y.io(M.cfg.wwwroot + this.url, {
+          method: 'POST',
+          data: this.data,
+          headers: {
+              //'Content-Type': 'application/json'
+          },
+          on: {
+              success: function (id, response) {
+              },
+              failure: function () {
+                popup.error();
+              }
+          }
+      });
+
+    },
+
+    run: function(callback){
+      this.data.sesskey = this.sesskey;
+      loadingIcon.show();
+      Y.io(M.cfg.wwwroot + this.url, {
+          method: 'POST',
+          data: this.data,
+          headers: {
+              //'Content-Type': 'application/json'
+          },
+          on: {
+              success: function (id, response) {
+                loadingIcon.remove();
+                let result = JSON.parse(response.responseText);
+                if (result.error) {
+                  popup.textError = result.errormsg;
+                  popup.error();
+                  return;
                 }
-            });
+                if (callback) callback();
 
-        },
+              },
+              failure: function () {
+                popup.error();
+              }
+          }
+      });
 
-        run: function (callback) {
-            this.data.sesskey = this.sesskey;
-            loadingIcon.show();
-            Y.io(M.cfg.wwwroot + this.url, {
-                method: 'POST',
-                data: this.data,
-                headers: {},
-                on: {
-                    success: function (id, response) {
-                        loadingIcon.remove();
-                        let result = JSON.parse(response.responseText);
-                        if (result.error) {
-                            popup.textError = result.errormsg;
-                            popup.error();
-                            return;
-                        }
-                        if (callback){callback()};
+    },
 
-                    },
-                    failure: function () {
-                        popup.error();
-                    }
-                }
-            });
+    runPopup: function(){
 
-        },
+      let result;
+      this.data.sesskey = this.sesskey;
 
-        runPopup: function () {
+      Y.io(M.cfg.wwwroot + this.url, {
+          method: 'POST',
+          data: this.data,
+          headers: {
+              //'Content-Type': 'application/json'
+          },
+          on: {
+              success: function (id, response) {
 
-            let result;
-            this.data.sesskey = this.sesskey;
+                // popup.text = response.responseText;
+                let result = JSON.parse(response.responseText);
+                popup.textHead = result.header;
+                popup.text = result.content;
+                popup.show();
+              },
+              failure: function () {
+                popup.error();
+              }
+          }
+      });
 
-            Y.io(M.cfg.wwwroot + this.url, {
-                method: 'POST',
-                data: this.data,
-                headers: {},
-                on: {
-                    success: function (id, response) {
-                        let result = JSON.parse(response.responseText);
-                        popup.textHead = result.header;
-                        popup.text = result.content;
-                        popup.show();
-                    },
-                    failure: function () {
-                        popup.error();
-                    }
-                }
-            });
+    },
 
-        },
+    setHTML: function(){
+      this.data.sesskey = this.sesskey;
+      const targetBlock = document.querySelector(this.data.target_block);
+      loadingIcon.show();
+      Y.io(M.cfg.wwwroot + this.url, {
+          method: 'POST',
+          data: this.data,
+          headers: {
+              //'Content-Type': 'application/json'
+          },
+          on: {
+              success: function (id, response) {
+                loadingIcon.remove();
+                popup.remove();
+                let result = JSON.parse(response.responseText);
+                targetBlock.innerHTML = result.content;
+              },
+              failure: function () {
+                popup.error();
+              }
+          }
+      });
+    },
 
-        setHTML: function () {
-            this.data.sesskey = this.sesskey;
-            const targetBlock = document.querySelector(this.data.target_block);
-            loadingIcon.show();
-            Y.io(M.cfg.wwwroot + this.url, {
-                method: 'POST',
-                data: this.data,
-                headers: {},
-                on: {
-                    success: function (id, response) {
-                        loadingIcon.remove();
-                        popup.remove();
-                        let result = JSON.parse(response.responseText);
-                        targetBlock.innerHTML = result.content;
-                    },
-                    failure: function () {
-                        popup.error();
-                    }
-                }
-            });
-        },
+  }
 
-    }
-
-    return ajax
+  return ajax
 
 });
--- amd/src/dragula.js
+++ amd/src/dragula.js
@@ -1,79 +1,83 @@
-define(['filter_teamwork/ajax', 'filter_teamwork/render'], function (ajax, render) {
-    `use strict`;
 
-    const renderPageAfterDrag = (el, callback) => {
+define(['filter_teamwork/ajax', 'filter_teamwork/render'], function(ajax, render) {
+`use strict`;
 
-        const allTeamsBlocks = Array.from(document.querySelectorAll(`div[data-team_id]`));
-        const allTeams = [];
-        const draguserid = el.dataset.student_id;
+  const renderPageAfterDrag = (el, callback) => {
 
-        let teamid;
-        allTeamsBlocks.forEach((item) => {
-            let team = {};
-            team.teamid = item.dataset.team_id;
-            team.studentid = [];
+    const allTeamsBlocks = Array.from(document.querySelectorAll(`div[data-team_id]`));
+    const allTeams = [];
+    const draguserid = el.dataset.student_id;
 
-            let allStudents = Array.from(item.querySelectorAll(`.teamwork_student`));
-            allStudents.forEach((student) => {
-                team.studentid.push(student.dataset.student_id);
-            });
-            allTeams.push(team);
-        });
+    let teamid;
+    allTeamsBlocks.forEach((item)=>{
+      let team = {};
+      team.teamid = item.dataset.team_id;
+      team.studentid = [];
 
-        ajax.data = {
-            draguserid: draguserid,
-            method: 'drag_student_card',
-            newTeams: JSON.stringify(allTeams),
-            activityid: render.data.activityid,
-            moduletype: render.data.moduletype,
-            courseid: render.data.courseid,
-            selectgroupid: render.data.selectgroupid
-        };
+      let allStudents = Array.from(item.querySelectorAll(`.teamwork_student`));
+      allStudents.forEach((student)=>{
+        team.studentid.push(student.dataset.student_id);
+      });
+      allTeams.push(team);
+    });
 
-        ajax.run(callback);
+    ajax.data =  {
+      draguserid: draguserid,
+      method: 'drag_student_card',
+      newTeams : JSON.stringify(allTeams),
+      activityid: render.data.activityid,
+      moduletype: render.data.moduletype,
+      courseid: render.data.courseid,
+      selectgroupid: render.data.selectgroupid
+    };
 
+    ajax.run(callback);
+
+  }
+
+  const checkOverflowAndRender = (el, target, source) => {
+
+    let maxCount = source.dataset.max_count;
+
+    if (target.childElementCount === maxCount) {
+      target.classList.add(`stop-drag`);
+    }
+    if (source.childElementCount < maxCount) {
+      source.classList.remove(`stop-drag`);
     }
 
-    const checkOverflowAndRender = (el, target, source) => {
+    renderPageAfterDrag(el, function(){
+      render.setDefaultData();
+      render.studentList();
+      render.teamsCard();
+    });
 
-        let maxCount = source.dataset.max_count;
+  }
 
-        if (target.childElementCount === maxCount) {
-            target.classList.add(`stop-drag`);
+  const drag = {
+
+    startDrag: function() {
+      dragula({
+        isContainer: function (el) {
+          return el.classList.contains(`draggable`);
+        },
+        accepts: function (el, target) {
+          // if (el.classList.contains(`stop-drag-item`)) {
+          //   return false;
+          // }
+          if (!target.classList.contains(`stop-drag`)) {
+            return target;
+          }
+
+        },
+        invalid: function (el, handle) {
+          if(el.classList.contains(`stop-drag-item`)){
+            return true;
+          }
         }
-        if (source.childElementCount < maxCount) {
-            source.classList.remove(`stop-drag`);
-        }
-
-        renderPageAfterDrag(el, function () {
-            render.setDefaultData();
-            render.studentList();
-            render.teamsCard();
-        });
-
+      }).on('drop', checkOverflowAndRender)
     }
-
-    const drag = {
-
-        startDrag: function () {
-            dragula({
-                isContainer: function (el) {
-                    return el.classList.contains(`draggable`);
-                },
-                accepts: function (el, target) {
-                    if (!target.classList.contains(`stop-drag`)) {
-                        return target;
-                    }
-
-                },
-                invalid: function (el, handle) {
-                    if (el.classList.contains(`stop-drag-item`)) {
-                        return true;
-                    }
-                }
-            }).on('drop', checkOverflowAndRender)
-        }
-    }
+  }
 
     return drag;
 });
